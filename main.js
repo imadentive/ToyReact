b101efbfa8767ff655b8d25de357f65ef36f8ad5
luoweilong <imadentive@qq.com>
2020-07-28 11:03:58 +0800

    简单的实现，没有render方法

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 import {ToyReact} from './ToyReact.js'
-class MyComponent {
+// class MyComponent {
 
-}
+// }
 // let a = <MyComponent name="a" />
 
 // MyComponent 和 div 小写是有区别的
 let a = <div name="a" id="ida">
-    <span></span>
-    <span></span>
-    <span></span>
+    <span>hello</span>
+    <span>world</span>
+    <span>！</span>
 </div>
 
 console.log(a)
+
+/*
+var a = _ToyReact.ToyReact.createElement("div", {
+    name: "a",
+    id: "ida"
+  }, 
+  _ToyReact.ToyReact.createElement("span", null, "hello"), 
+  _ToyReact.ToyReact.createElement("span", null, "world"), 
+  _ToyReact.ToyReact.createElement("span", null, "\uFF01"))
+
+  */
 document.body.appendChild(a)
 // var a = createElement(MyComponent, {
 //     name: "a"
